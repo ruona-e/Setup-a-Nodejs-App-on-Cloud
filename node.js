56438f118e9f2de51cfdc777098ff1305812386f
Ogheneruona Emanor <59610175+ruona-e@users.noreply.github.com>
2023-10-03 15:28:02 +0100

    Update node.js

--- node.js
+++ node.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
